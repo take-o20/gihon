b2bc020f8fd51f58411caefc9b879fd8a0ec11cc
takeo <takeo@ubuntu-22.04.1-TLS.myguest.virtualbox.org>
2022-10-17 22:40:46 +0900

    donnyokuhou

---
+++ ch2/ch2-2/kouka.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+const int V[6] = {1, 5, 10, 50, 100, 500};
+
+int C[6];
+int A;
+
+void solve() {
+    int ans = 0;
+
+    for(int i = 5; i >= 0; i--) {
+        int t = min(A / V[i], C[i]);
+        A -= t * V[i];
+        ans += t;
+    }
+    cout << ans << endl;
+}
